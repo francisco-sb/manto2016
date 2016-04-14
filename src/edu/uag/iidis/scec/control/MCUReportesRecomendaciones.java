e742120c5193a7189867e372aa1192d574ff6684
jorgeluis9412 <luis_941207@hotmail.com>
2016-04-14 11:49:03 -0500

    Reporte de recomendaciones
    Control - Rrecomendaciones

---
+++ src/edu/uag/iidis/scec/control/MCUReportesRecomendaciones.java
@@ -0,0 +1,76 @@
+package edu.uag.iidis.scec.control;
+
+import edu.uag.iidis.scec.vista.*;
+import edu.uag.iidis.scec.modelo.*;
+import edu.uag.iidis.scec.servicios.*;
+
+import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.MappingDispatchAction;
+
+
+
+public final class MCUReportesRecomendaciones
+        extends MappingDispatchAction {
+
+    private Log log = LogFactory.getLog(MCURegistrarUsuario.class);
+
+
+    public ActionForward solicitarReportesRecomendaciones(
+                ActionMapping mapping,
+                ActionForm form,
+                HttpServletRequest request,
+                HttpServletResponse response)
+            throws Exception {
+
+        if (log.isDebugEnabled()) {
+            log.debug(">solicitarReportesRecomendaciones");
+        }
+
+        // Verifica si la acción fue cancelada por el usuario
+        if (isCancelled(request)) {
+            if (log.isDebugEnabled()) {
+                log.debug("<La acción fue cancelada");
+            }
+            return (mapping.findForward("cancelar"));
+        }
+
+        FormaReporteRecomendaciones forma = (FormaReporteRecomendaciones)form;
+
+        ManejadorRecomendaciones mr = new ManejadorRecomendaciones();
+        Collection resultado = mr.listarRecomendaciones();
+		
+		ManejadorLugares mr2 = new ManejadorLugares();
+        Collection resultado2 = mr2.listarLugares();
+		forma.setLugares(resultado2);
+
+        ActionMessages errores = new ActionMessages();
+        if (resultado != null) {
+            if ( resultado.isEmpty() ) {
+                errores.add(ActionMessages.GLOBAL_MESSAGE,
+                    new ActionMessage("errors.registroVacio"));
+                saveErrors(request, errores);
+            } else {
+                forma.setRecomendacion( resultado );
+            }
+            return (mapping.findForward("exito"));
+        } else {
+            log.error("Ocurrió un error de infraestructura");
+            errores.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("errors.infraestructura"));                
+            saveErrors(request, errores);
+            return ( mapping.findForward("fracaso") );
+        }
+
+    }
+}
